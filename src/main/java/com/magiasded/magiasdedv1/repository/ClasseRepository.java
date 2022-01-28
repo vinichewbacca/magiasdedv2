afbebf314a52afa39b83c6b9e5e7720dc7f9dcec
viniciusChewbacca <vas.aranha@gmail.com>
2022-01-28 12:46:56 -0400

    primeira versão testando para ver se sobe para o git hub

---
+++ src/main/java/com/magiasded/magiasdedv1/repository/ClasseRepository.java
@@ -0,0 +1,9 @@
+package com.magiasded.magiasdedv1.repository;
+
+import com.magiasded.magiasdedv1.model.Classe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClasseRepository extends JpaRepository <Classe, Long>
+{
+
+}
