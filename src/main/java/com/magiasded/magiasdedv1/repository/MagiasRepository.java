79de025c408712d76d1ba227698d6c10ac2d8bd5
vinichewbacca <vinicius.souza@estudante.ifms.edu.br>
2022-01-28 18:45:11 -0400

    Criado no pc em casa

---
+++ src/main/java/com/magiasded/magiasdedv1/repository/MagiasRepository.java
@@ -0,0 +1,9 @@
+package com.magiasded.magiasdedv1.repository;
+
+import com.magiasded.magiasdedv1.model.Magias;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MagiasRepository extends JpaRepository <Magias, Long>
+{
+
+}
